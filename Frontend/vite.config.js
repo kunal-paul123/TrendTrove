b0e9379fd1ae5ffc5133cd5fc8053f50b75b8fc9
Kunal Paul <kp2346578@gmail.com>
2026-01-25 12:01:26 +0530

    done

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://trendtrove-5zd0.onrender.com",
+        target: "http://localhost:5000",
         changeOrigin: true,
         secure: false,
       },
